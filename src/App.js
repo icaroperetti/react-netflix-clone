3c45ca70fd7420b288f0d2a3978f238782067883
icaroperetti <icaroperetti50@gmail.com>
2021-08-18 11:07:46 -0300

    Criando componente de filme em destaque

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React,{useEffect,useState}from "react"
 import api from "./api"
-import MovieRow from "./components/MovieRow/MovieRow.js"
+
+import MovieRow from "./components/MovieRow/MovieRow"
+import FeaturedMovie from "./components/MovieRow/FeaturedMovie/FeaturedMovie"
+
 import './App.css'
 
 export default function App()  {
 
   const [movieList,setMovieList] = useState([])
+  const [featuredMovie,setFeaturedMovie] = useState(null)
 
   useEffect(() =>{
     const loadAll = async () => {
       //Get movie list
       let list = await api.getHomeList()
       setMovieList(list)
+
+      //Get featured movie
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await api.getMovieInfo(chosen.id, 'tv')
+
+      setFeaturedMovie(chosenInfo)
+     
     }
     loadAll()
   },[])
 
   return (
     <div className="page">
+
+      {featuredMovie &&
+        <FeaturedMovie item={featuredMovie} />
+      }
+
       <section className="lists">
         {movieList.map((item,key) => (
          <MovieRow key={key} title={item.title} items={item.items}/>
